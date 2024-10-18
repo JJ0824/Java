64b23bbe84182d125b8516e441a4f514144e68bb
JJ0824 <auxjungjae@gmail.com>
2024-10-18 11:44:45 +0900

    처음 업데이트

---
+++ Test1/src/TypeChange.java
@@ -0,0 +1,15 @@
+public class TypeChange {
+    public static void main(String[] args) {
+        //int value1 = (int)5.3;
+        // 타입 캐스팅
+        int value1 = (int)5.99; // 결과 5
+        System.out.println(value1);
+        //long value2 = (long)10;
+        long value2 = 10L;
+        //float value3 = (float)5.8;
+        float value3 = 5.8F;
+        double value4 = (double)16; // 안 붙여도 무방
+        System.out.println(value4);
+
+    }
+}

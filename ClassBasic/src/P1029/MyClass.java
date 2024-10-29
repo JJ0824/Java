e70b4dac7e4855eb01304f77ed9184686e25baef
JJ0824 <auxjungjae@gmail.com>
2024-10-29 12:51:32 +0900

    화요일

---
+++ ClassBasic/src/P1029/MyClass.java
@@ -0,0 +1,35 @@
+package P1029;
+
+public class MyClass {
+    public static void main(String[] args) {
+        Student steve = new Student("Steve", 25);
+        Student tom = new Student();
+        Student sam = new Student("Sam", 29, "대전",
+                "남", 100, 90, 80);
+        System.out.println(steve.name);
+        System.out.println(steve.address);
+        System.out.println(tom.name);
+        System.out.println(sam.name);
+        System.out.println(sam.scoreHistory);
+        // 메서드의 사용
+        // 외부 클래스가 가지고 있는 메서드 이므로 해당 클래스의 인스턴스를 명시 해야함!!
+        System.out.println(steve.sumScore());
+        System.out.println(tom.sumScore());
+        System.out.println(sam.sumScore());
+
+        User kim = new User("Kim", "kim@gmail.com", "1234");
+        System.out.println(kim.password);
+        kim.setPassword("abcd");
+        System.out.println(kim.password);
+
+        Product book1 = new Product("A001", "여행", 10000, 100);
+        System.out.println(book1.price + " " + book1.stock);
+        book1.setPrice(11000);
+        book1.setStock(90);
+        System.out.println(book1.price + " " + book1.stock);
+    }
+
+}
+//class Student {
+//
+//}

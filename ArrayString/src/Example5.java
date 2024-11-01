6121da234ecafbb2f6e70cbe34b09a57d05f169f
JJ0824 <auxjungjae@gmail.com>
2024-11-01 16:13:04 +0900

    up

--- ArrayString/src/Example5.java
+++ ArrayString/src/Example5.java
@@ -30,7 +30,7 @@ public class Example5 {
     public static String getCharRatio(String str) {
         int lowerCount = 0; // 소문자 갯수
         int upperCount = 0; // 대문자 갯수
-          char[] chArray = str.toCharArray();// String을 char배열로 변환
+        char[] chArray = str.toCharArray();// String을 char배열로 변환
         for(int i=0; i<chArray.length; i++ ) {
               if (str.charAt(i) >= 'a'&& str.charAt(i) <= 'z') {
                   lowerCount = lowerCount + 1;
